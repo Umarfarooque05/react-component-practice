2729c1414094679171a3a7f040bb15e191c0e30e
Umar Farooque <80164102+Umarfarooque05@users.noreply.github.com>
2022-04-03 05:27:35 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import React from "react";
+import Heading from "./Heading.";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+export default App;
